314e2108de80538d12dd0ae73ac9d46b4807b0b2
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-02-23 00:29:30 +0000

    Header Completed

--- index.php
+++ index.php
@@ -1,43 +1,120 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.80.0">
-    <title>Starter Template · Bootstrap v5.0</title>
-    <!-- Bootstrap core CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="mystyle.css" rel="stylesheet">
-  </head>
-  <body>
-<main class="container phone-display">
-  <div class="starter-template text-center">
-    <div class="row align-items-start">
-    <div class="col-5 phone1">
-     <h2>Phone 1</h2>
-     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
-     <button type="button" class="btn btn-dark">Calculate</button>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.80.0">
+  <title>Fantasy Home Page</title>
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="./myStyle.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row align-items-center">
+      <div class="col-12">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid">
+            <a class="navbar-brand d-inline-block nav-link disabled d-xl-none" href="">Club Logos</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerLogo" aria-controls="navbarTogglerLogo" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerLogo">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/1.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;&nbsp;<p class="d-inline-block d-xl-none">Athlone Town</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/2.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Bohemians</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/3.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Bray Wanderers</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/4.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Cobh Ramblers</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/5.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Cork City</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/6.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Derry City</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/7.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">DLR Waves FC</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/8.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Drogheda United</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/9.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Dundalk</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/10.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Finn Harps</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/11.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Galway United</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/12.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Kerry FC</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/13.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Longford Town</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/14.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Peamount United FC</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/15.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Shamrock Rovers</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/16.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Shelbourne</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/17.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Sligo Rovers</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/18.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">St. Patrick's Athletic</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/19.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Treaty United</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/20.png" alt="Logo" width="30" height="30  " class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">UCD AFC</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/21.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Waterford</p></a></li>
+                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/22.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Wexford FC</p></a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="col-12">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#" style="color: white;">
+              <img src="./images/logo.png" id="logo" alt="Logo" width="30" height="24" class="d-inline-block">
+              Ireland Fantasy
+            </a>
+            <!-- <a class="navbar-brand d-inline-block nav-link disabled" href="">Navbar</a> -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" style="color: white;" href="#">Contact-Us</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" style="color: white;" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Translate
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <button type="button" class="btn btn-primary">PLAY NOW</button>
+                </li>
+              </ul>
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn bg-info" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
-    <div class="col-2">
+    <div class="row align-items-center">
+      <div class="col-12">
+        <nav class="navbar navbar-expand navbar-light bg-light">
+          <div class="container-fluid d-flex justify-content-around">
+            <div class="navbar-nav">
+              <a class="nav-link check" href="#">Teams</a>
+              <a class="nav-link check" href="#">League Table</a>
+              <a class="nav-link check" href="#">Fixtures</a>
+              <a class="nav-link check" href="#">Statistics</a>
+              <a class="nav-link check" href="#">Players</a>
+            </div>
+          </div>
+        </nav>
+
+      </div>
     </div>
-    <div class="col-5 phone2">
-      <h2>Phone 2</h2>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
-      <section class="white-box">
-        Lorem ipsum dolor sit<br>
-        Lorem ipsum dolor sit<br>
-        Lorem ipsum dolor sit<br>
-        Lorem ipsum dolor sit<br>
-      <button type="button" class="btn btn-dark">Recalculate</button>
-    </section>
-    </div>
-  </div>
+
   </div>
 
-</main><!-- /.container -->
-    <script src="js/bootstrap.bundle.min.js"></script>
-  </body>
+  <main class="container">
+    asedfghfdsadfghnv
+  </main><!-- /.container -->
+  <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
 </html>
