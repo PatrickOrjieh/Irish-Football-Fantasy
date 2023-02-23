9cde9d2041eb675855eda2bac871d7d7e16fa165
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-02-23 03:01:16 +0000

    Footer And Carousel Added

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
   <title>Fantasy Home Page</title>
   <!-- Bootstrap core CSS -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
   <!-- Custom styles for this template -->
   <link href="./myStyle.css" rel="stylesheet">
 </head>
@@ -111,10 +112,74 @@
 
   </div>
 
-  <main class="container">
-    asedfghfdsadfghnv
+  <main class="container-fluid">
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./images/caro5.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./images/caro4.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./images/error3.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
   </main><!-- /.container -->
+  <div class="b-example-divider"></div>
+  <div class="container-fluid footer">
+    <footer class="py-5">
+      <div class="row">
+        <div class="col-6 col-md-3 mb-3">
+          <h5>Links</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Teams</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Fixtures</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Statistics</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Players</a></li>
+          </ul>
+        </div>
+
+        <div class="col-6 col-md-3 mb-3 address">
+          <h5>Our Address</h5>
+          <address>
+            8, Oriel House<br />
+            Dublin Road, Dundalk<br />
+            County Louth<br />
+            <span class="fa fa-phone"></span>: +353 85 2155 782<br />
+            <span class="fa fa-fax"></span>: +353 85 2155 782<br />
+            <span class="fa fa-envelope"></span>: <a class="link-primary" href="mailto:d00251785@student.dkit.ie">d00251785@student.dkit.ie</a>
+          </address>
+        </div>
+
+        <div class="col-md-5 offset-md-1 mb-3">
+          <form>
+            <h5>Subscribe to our newsletter</h5>
+            <p>Monthly digest of what's new and exciting from us.</p>
+            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+              <label for="newsletter1" class="visually-hidden">Email address</label>
+              <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+              <button class="btn bg-info" type="button">Subscribe</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+        <p class="text-muted" style="color: white;">© 2023 FAI & Patrick Orjieh, Inc. All rights reserved.</p>
+        <ul class="list-unstyled d-flex">
+          <li><a class="text-muted" href="#"><i class="fa fa-facebook af"></i></a></li>
+          <li><a class="text-muted" href="#"><i class="fa fa-twitter af"></i></a></li>
+          <li><a class="text-muted" href="#"><i class="fa fa-instagram af"></i></a></li>
+          <li><a class="text-muted" href="#"><i class="fa fa-linkedin af"></i></a></li>
+        </ul>
+      </div>
+    </footer>
+  </div>
   <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 </html>
