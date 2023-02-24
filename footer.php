a1854d7a636fb58f0f9b10b4e743c9a27ff978be
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-02-24 01:35:30 +0000

    Home Page Design Completed

---
+++ footer.php
@@ -0,0 +1,50 @@
+<div class="container-fluid footer">
+    <footer class="py-5">
+      <div class="row">
+        <div class="col-6 col-md-3 mb-3">
+          <h5>Links</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Teams</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Fixtures</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Statistics</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Players</a></li>
+          </ul>
+        </div>
+
+        <div class="col-6 col-md-3 mb-3 address">
+          <h5>Our Address</h5>
+          <address>
+            8, Oriel House<br />
+            Dublin Road, Dundalk<br />
+            County Louth<br />
+            <span class="fa fa-phone"></span>: +353 85 2155 782<br />
+            <span class="fa fa-fax"></span>: +353 85 2155 782<br />
+            <span class="fa fa-envelope"></span>: <a class="link-primary" href="mailto:d00251785@student.dkit.ie">d00251785@student.dkit.ie</a>
+          </address>
+        </div>
+
+        <div class="col-md-5 offset-md-1 mb-3">
+          <form>
+            <h5>Subscribe to our newsletter</h5>
+            <p>Monthly digest of what's new and exciting from us.</p>
+            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+              <label for="newsletter1" class="visually-hidden">Email address</label>
+              <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+              <button class="btn bg-info" type="button">Subscribe</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+        <p class="text-muted" style="color: white;">© 2023 FAI & Patrick Orjieh, Inc. All rights reserved.</p>
+        <ul class="list-unstyled d-flex">
+          <li><a class="text-muted" href="#"><i class="fa fa-facebook af"></i></a></li>
+          <li><a class="text-muted" href="#"><i class="fa fa-twitter af"></i></a></li>
+          <li><a class="text-muted" href="#"><i class="fa fa-instagram af"></i></a></li>
+          <li><a class="text-muted" href="#"><i class="fa fa-linkedin af"></i></a></li>
+        </ul>
+      </div>
+    </footer>
+  </div>
--- index.php
+++ index.php
@@ -16,168 +16,101 @@
 </head>
 
 <body>
-  <div class="container-fluid">
-    <div class="row align-items-center">
-      <div class="col-12">
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <div class="container-fluid">
-            <a class="navbar-brand d-inline-block nav-link disabled d-xl-none" href="">Club Logos</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerLogo" aria-controls="navbarTogglerLogo" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerLogo">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/1.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;&nbsp;<p class="d-inline-block d-xl-none">Athlone Town</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/2.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Bohemians</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/3.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Bray Wanderers</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/4.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Cobh Ramblers</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/5.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Cork City</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/6.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Derry City</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/7.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">DLR Waves FC</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/8.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Drogheda United</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/9.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Dundalk</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/10.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Finn Harps</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/11.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Galway United</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/12.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Kerry FC</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/13.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Longford Town</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/14.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Peamount United FC</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/15.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Shamrock Rovers</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/16.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Shelbourne</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/17.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Sligo Rovers</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/18.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">St. Patrick's Athletic</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/19.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Treaty United</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/20.png" alt="Logo" width="30" height="30  " class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">UCD AFC</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/21.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Waterford</p></a></li>
-                <li class="nav-item"><a class="nav-link logo_image" href="#"><img src="./images/badges/22.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">&nbsp;<p class="d-inline-block d-xl-none">Wexford FC</p></a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>
-      <div class="col-12">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#" style="color: white;">
-              <img src="./images/logo.png" id="logo" alt="Logo" width="30" height="24" class="d-inline-block">
-              Ireland Fantasy
-            </a>
-            <!-- <a class="navbar-brand d-inline-block nav-link disabled" href="">Navbar</a> -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link" aria-current="page" style="color: white;" href="#">Contact-Us</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" style="color: white;" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Translate
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <button type="button" class="btn btn-primary">PLAY NOW</button>
-                </li>
-              </ul>
-              <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn bg-info" type="submit">Search</button>
-              </form>
-            </div>
-          </div>
-        </nav>
-      </div>
-    </div>
-    <div class="row align-items-center">
-      <div class="col-12">
-        <nav class="navbar navbar-expand navbar-light bg-light">
-          <div class="container-fluid d-flex justify-content-around">
-            <div class="navbar-nav">
-              <a class="nav-link check" href="#">Teams</a>
-              <a class="nav-link check" href="#">League Table</a>
-              <a class="nav-link check" href="#">Fixtures</a>
-              <a class="nav-link check" href="#">Statistics</a>
-              <a class="nav-link check" href="#">Players</a>
-            </div>
-          </div>
-        </nav>
-
-      </div>
-    </div>
-
-  </div>
+  <?php require('header.php'); ?>
 
   <main class="container-fluid">
-  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./images/caro5.png" class="d-block w-100" alt="...">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-bs-interval="4000">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="./images/kk3.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./images/caro5.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./images/error3.png" class="d-block w-100" alt="...">
+        </div>
+      </div>
     </div>
-    <div class="carousel-item">
-      <img src="./images/caro4.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="./images/error3.png" class="d-block w-100" alt="...">
-    </div>
-  </div>
-</div>
-  </main><!-- /.container -->
-  <div class="b-example-divider"></div>
-  <div class="container-fluid footer">
-    <footer class="py-5">
+    <div class="container sign mt-5 mb-3">
       <div class="row">
-        <div class="col-6 col-md-3 mb-3">
-          <h5>Links</h5>
-          <ul class="nav flex-column">
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Home</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Teams</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Fixtures</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Statistics</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 same">Players</a></li>
-          </ul>
-        </div>
-
-        <div class="col-6 col-md-3 mb-3 address">
-          <h5>Our Address</h5>
-          <address>
-            8, Oriel House<br />
-            Dublin Road, Dundalk<br />
-            County Louth<br />
-            <span class="fa fa-phone"></span>: +353 85 2155 782<br />
-            <span class="fa fa-fax"></span>: +353 85 2155 782<br />
-            <span class="fa fa-envelope"></span>: <a class="link-primary" href="mailto:d00251785@student.dkit.ie">d00251785@student.dkit.ie</a>
-          </address>
-        </div>
-
-        <div class="col-md-5 offset-md-1 mb-3">
-          <form>
-            <h5>Subscribe to our newsletter</h5>
-            <p>Monthly digest of what's new and exciting from us.</p>
-            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-              <label for="newsletter1" class="visually-hidden">Email address</label>
-              <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
-              <button class="btn bg-info" type="button">Subscribe</button>
+        <div class="col-12">
+          <form action="">
+            <div class="form-group row">
+              <h2 class="col-12 col-lg-1 mb-3" style="color: #212529; font-size: 17px; font-weight: bolder;">Sign In</h2>
+              <label for="email" class="col-4 col-lg-1 mb-3 col-form-label">Email</label>
+              <div class="col-8 col-lg-3">
+                <input type="text" class="form-control input-decoration" id="email" name="email" placeholder="Email">
+              </div>
+              <label for="password" class="col-4 mb-3 col-lg-1 col-form-label">Password</label>
+              <div class="col-8 col-lg-3">
+                <input type="text" class="form-control input-decoration" id="password" name="password" placeholder="Password">
+              </div>
+              <div class="offset-4 offset-lg-0 col-8 col-lg-3">
+                <button type="submit" class="btn bg-info">Sign In</button>
+                &nbsp;
+                <a href="" style="text-decoration: none;">Forgot Password?</a>
+              </div>
             </div>
           </form>
         </div>
-      </div>
+        <div class="row mt-5 mb-4">
+          <div class="col text-center position-relative">
+            <hr style="border-top: 1px solid black; width: 100%; margin: auto;">
+            <span class="bg-white px-3 position-absolute" style="color:#212529;top: -12px; left: 50%; transform: translateX(-50%);">Or login with</span>
+          </div>
+        </div>
+        <div class="row" style="flex-wrap: wrap;">
+          <div class="col text-center">
+            <a href="" class="btn btn-primary btn-lg mt-1" style="background-color: #3b5998; border-color: #3b5998;"><i class="fa fa-facebook"></i> Facebook</a>
 
-      <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-        <p class="text-muted" style="color: white;">© 2023 FAI & Patrick Orjieh, Inc. All rights reserved.</p>
-        <ul class="list-unstyled d-flex">
-          <li><a class="text-muted" href="#"><i class="fa fa-facebook af"></i></a></li>
-          <li><a class="text-muted" href="#"><i class="fa fa-twitter af"></i></a></li>
-          <li><a class="text-muted" href="#"><i class="fa fa-instagram af"></i></a></li>
-          <li><a class="text-muted" href="#"><i class="fa fa-linkedin af"></i></a></li>
-        </ul>
+            <a href="" class="btn btn-primary btn-lg mt-1" style="background-color: #dd4b39; border-color: #dd4b39;"><i class="fa fa-google"></i> Google</a>
+
+            <a href="" class="btn btn-primary btn-lg mt-1" style="background-color: #0077b5; border-color: #0077b5;"><i class="fa fa-linkedin"></i> LinkedIn</a>
+
+            <a href="" class="btn btn-primary btn-lg mt-1" style="background-color: #1da1f2; border-color: #1da1f2;"><i class="fa fa-twitter"></i> Twitter</a>
+          </div>
+        </div>
       </div>
-    </footer>
-  </div>
+    </div>
+
+    <div class="container mt-4 mb-4">
+      <div class="row align-items-center" style="background-color: #212529;">
+        <div class="col-12 col-sm-3 text-center mb-2 p-3" style="font-weight: bold; font-size:20px;">
+          <img src="./images/logo.png" style="border-radius: 50%; height:2rem; width: 2rem;" alt=""> Fantasy
+        </div>
+        <div class="col-12 col-sm-6 mb-2 p-3">
+          <h4 style="color: #0dcaf0;">Ready to Play Irish Premier Fantasy</h4>
+          <p>With over 9 million players, Fantasy Premier League is the biggest Fantasy
+            Football game in the world. <span style="font-weight: bold;">It’s FREE to play and you can win great prizes!</span> </p>
+        </div>
+        <div class="col-12 col-sm-3 mb-2 text-center d-grid gap-2  mx-auto">
+          <!-- <button type="submit" class="btn bg-info p-2">Sign Up Now</button> -->
+          <button type="submit" class="btn bg-info p-2" style="font-size: small; font-weight: bold;">Sign Up Now</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="container" style="color:#212529;">
+      <div class="row row-content align-items-center">
+        <div class="col-12 col-sm-4 order-sm-last col-md-3 text-center">
+          <h3>Meet our Creator</h3>
+        </div>
+        <div class="col col-sm order-sm-first col-md">
+          <div class="media">
+            <img src="./images/me.jpg" alt="alberto" class="d-flex mr-3 img-thumbnail align-self-center">
+            <div class="media-body">
+              <h2 class="mt-0">Patrick Orjieh</h2>
+              <h4>Executive Chief</h4>
+              <p class="d-none d-sm-block">Patrick Orjieh, the renowned award-winning developer, has been making waves in the tech world with his cutting-edge innovations, inspiring countless others to follow in his footsteps. His relentless pursuit of excellence and unwavering passion for coding has led him to create some of the most remarkable software applications in the industry, earning him numerous accolades and recognition from his peers. Whether it's building complex algorithms or designing intuitive user interfaces, Patrick always goes above and beyond to deliver exceptional results, cementing his status as a true visionary in the world of technology.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main><!-- /.container -->
+  <div class="b-example-divider"></div>
+  <?php require('footer.php'); ?>
   <script src="js/bootstrap.bundle.min.js"></script>
   <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
